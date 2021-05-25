ccf4136aec5b7d5da8993cbfe2bd2eb156a0262a
julianruben <julian.ruben@stellantis.com>
2021-05-25 15:24:59 +0000

    Lab Exercise 18

---
+++ API/restfulAPI/iceAndFire01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Alta3 Research - Exploring OpenAPIs with requests"""
+# documentation for this API is at
+# https://anapioficeandfire.com/Documentation
+
+import requests
+import pprint
+
+AOIF = "https://www.anapioficeandfire.com/api/books"
+
+def main():
+    ## Send HTTPS GET to the API of ICE and Fire
+    gotresp = requests.get(AOIF)
+
+    ## Decode the response
+    got_dj = gotresp.json()
+
+    ## print the response
+    pprint.pprint(got_dj)
+
+if __name__ == "__main__":
+    main()
+
